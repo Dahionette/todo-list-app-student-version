7389894c588c315c84c80b2cf699328f3a250831
Dahionette <dahionetter2789@nycstudents.net>
2025-06-06 12:24:27 -0400

    Update tasks.js

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,13 +3,13 @@ const router = express.Router();
 // There is a bug in line 4 you need to fix it
 const taskModel = require("../models/taskModel");
 
-//Write a comment describing the purpose of this route
+//This fuction brings back records for us to read it
 router.get("/", async (req, res) => {
   const tasks = await taskModel.getTasks();
   res.json(tasks);
 });
 
-// Write a comment describing the purpose of this route
+// This fuction creates the records in the database
 router.post("/", async (req, res) => {
   //there is a bug in line 15 you need to fix
   const { title, description } = req.body;
